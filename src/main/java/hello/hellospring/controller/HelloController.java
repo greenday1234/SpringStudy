e3c6d7f9cf3685327c5081ebf2a8a7622ab00af4
Lee ChanHui <ing3616062@naver.com>
2023-07-23 00:52:28 +0900

    웹 개발 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -35,11 +35,9 @@ public class HelloController {
     }
     static class Hello{
         private String name;
-
         public String getName() {
             return name;
         }
-
         public void setName(String name) {
             this.name = name;
         }
