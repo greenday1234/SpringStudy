5b8d3a4ad21671f14e2e55a21cd15deaa537f340
Lee ChanHui <ing3616062@naver.com>
2023-07-24 03:29:01 +0900

    회원 관리 예제

--- src/test/java/hello/hellospring/service/MemberServiceTest.java
+++ src/test/java/hello/hellospring/service/MemberServiceTest.java
@@ -31,6 +31,7 @@ class MemberServiceTest {
         //given
         Member member = new Member();
         member.setName("hello");
+
         //when
         Long saveId = memberService.join(member);
 
@@ -51,15 +52,14 @@ class MemberServiceTest {
         //when
         memberService.join(member1);
         IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
-
         assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
 //        try{
 //            memberService.join(member2);
 //            fail();
 //        } catch(IllegalStateException e){
 //            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123");
 //        }
-
 
         //then
     }
