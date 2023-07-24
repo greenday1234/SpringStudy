a9414298594dcd4e61bf221d76f7e986c764be5c
Lee ChanHui <ing3616062@naver.com>
2023-07-25 00:15:10 +0900

    스프링 빈과 의존관계

--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -3,6 +3,8 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
@@ -18,11 +20,6 @@ public class MemberService {
     //회원가입
     public long join(Member member){
         //같은 이름이 있는 중복 회원은 안된다는 조건
-//        Optional<Member> result = memberRepository.findByName(member.getName());
-//        Member member1 = result.get();
-//        result.ifPresent(m -> {
-//            throw new IllegalStateException("이미 존재하는 회원입니다.");
-//        });
         validateDuplicateMember(member);    //중복 회원 검증
 
         memberRepository.save(member);
