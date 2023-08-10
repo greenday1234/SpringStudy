afa4026db25da869de47300b7a79c0fede1f5810
Lee ChanHui <ing3616062@naver.com>
2023-08-10 23:15:23 +0900

    주문과 할인

---
+++ src/main/java/hello/core/discount/FixDiscountPolicy.java
@@ -0,0 +1,20 @@
+package hello.core.discount;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+
+public class FixDiscountPolicy implements DiscountPolicy{
+
+    private int discountFixAmount = 1000; //1000원 할인
+
+    @Override
+    public int discount(Member member, int price) {
+        //Enum Type은 == 사용
+        if(member.getGrade() == Grade.VIP){
+            return discountFixAmount;
+        }
+        else {
+            return 0;
+        }
+    }
+}
