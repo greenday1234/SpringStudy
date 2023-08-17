afe9c183b7b48259e792d6e4fad7196a28eb5470
Lee ChanHui <ing3616062@naver.com>
2023-08-17 15:24:49 +0900

    주석

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -14,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationContextBasicFindTest {
 
+    //구현체
     AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
 
     @Test
