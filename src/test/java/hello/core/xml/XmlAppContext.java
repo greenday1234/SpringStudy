4327a4339e0551d92d40f03c7be98e92858cb4b5
Lee ChanHui <ing3616062@naver.com>
2023-08-19 14:52:42 +0900

    주석

--- src/test/java/hello/core/xml/XmlAppContext.java
+++ src/test/java/hello/core/xml/XmlAppContext.java
@@ -11,6 +11,7 @@ import static org.assertj.core.api.Assertions.*;
 public class XmlAppContext {
 
     @Test
+    //기존 AppConfig을 참조하는 것이 아닌 xml파일을 참조하는 것으로 변경, GenericXmlApplicationContext 사용(인터페이스 사용)
     void XmlAppContext(){
         ApplicationContext ac = new GenericXmlApplicationContext("appConfig.xml");
         MemberService memberService = ac.getBean("memberService", MemberService.class);
