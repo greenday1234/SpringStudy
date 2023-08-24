49660e4b4a32d2d76082d933174833aa07e3dd8b
Lee ChanHui <ing3616062@naver.com>
2023-08-24 23:55:14 +0900

    ConfigurationSingletonTest

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -17,4 +17,9 @@ public class MemberServiceImpl implements MemberService {
     public Member findMember(Long memberId) {
         return memberRepository.findById(memberId);
     }
+
+    //Test 용도
+    public MemberRepository getMemberRepository(){
+        return memberRepository;
+    }
 }
