a2acbc80281b0efafef1192f5d229998660d1753
Lee ChanHui <ing3616062@naver.com>
2023-08-25 18:02:48 +0900

    ComponentScan

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired  //ac.getBean(MemberRepository.class) <- 자동으로 이 코드가 들어간다고 보면 된다.
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
