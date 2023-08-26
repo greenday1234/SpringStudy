3aec50c88d1ee3ec08e40a69bf9a3ee4bc355421
Lee ChanHui <ing3616062@naver.com>
2023-08-26 22:11:39 +0900

    주석

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
 
 @Configuration
+//Configuration Filter
 @ComponentScan(
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
