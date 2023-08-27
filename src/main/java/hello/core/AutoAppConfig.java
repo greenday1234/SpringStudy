8f4bc2b33fdf57752ca082421a2de05197fdecc1
Lee ChanHui <ing3616062@naver.com>
2023-08-27 22:43:34 +0900

    basePackages

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.FilterType;
 @Configuration
 //Configuration Filter
 @ComponentScan(
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
--- src/main/java/hello/core/CoreApplication.java
+++ src/main/java/hello/core/CoreApplication.java
@@ -3,7 +3,7 @@ package hello.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//스프링부트
+//스프링을 자동으로 생성할 때 기본으로 생성되는 클래스
 @SpringBootApplication
 public class CoreApplication {
 
