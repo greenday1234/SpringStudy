9a6cb8a70e8add7f06d4d7cb6d08d20e91cc8dbc
Lee ChanHui <ing3616062@naver.com>
2023-09-08 23:45:16 +0900

    Provider

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,9 +2,12 @@ package hello.core.scope;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import jakarta.inject.Provider;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
@@ -30,28 +33,27 @@ public class SingletonWithPrototypeTest1 {
         ClientBean clientBean1 = ac.getBean(ClientBean.class);
         int count1 = clientBean1.logic();
         assertThat(count1).isEqualTo(1);
-
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     //singleton은 굳이 안적어도 됨
     @Scope("singleton")
     static class ClientBean{
-        private final PrototypeBean prototypeBean;  //생성 시점에 주입
 
+        //테스트이기 때문에 필드주입 사용
+        //ObjectFactory<> 사용 가능
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
+        private Provider<PrototypeBean> prototypeBeanProvider;
 
-        public int logic(){
+        public int logic() {
+            PrototypeBean prototypeBean = prototypeBeanProvider.get();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
         }
-    }
+}
 
     @Scope("prototype")
     static class PrototypeBean{
