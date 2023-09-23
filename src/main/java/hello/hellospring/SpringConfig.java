1fbecd97933ddc167bbc24d90d64c6a479899172
Lee ChanHui <ing3616062@naver.com>
2023-09-23 18:50:13 +0900

    주석

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,31 +1,13 @@
 package hello.hellospring;
 
-import hello.hellospring.aop.TimeTraceAop;
 import hello.hellospring.repository.*;
 import hello.hellospring.service.MemberService;
-import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import javax.sql.DataSource;
-
 @Configuration
 public class SpringConfig {
-
-//    DataSource dataSource;
-//
-//    @Autowired
-//    public SpringConfig(DataSource dataSource) {
-//        this.dataSource = dataSource;
-//    }
-
-//    private EntityManager em;
-//
-//    @Autowired
-//    public SpringConfig(EntityManager em) {
-//        this.em = em;
-//    }
 
     private final MemberRepository memberRepository;
 
@@ -40,12 +22,4 @@ public class SpringConfig {
     public MemberService memberService(){
         return new MemberService(memberRepository);
     }
-
-//    @Bean
-//    public MemberRepository memberRepository(){
-//        //return new MemoryMemberRepository();
-//        //return new JdbcMemberRepository(dataSource);
-//        //return new JdbcTemplateMemberRepository(dataSource);
-//        //return new JpaMemberRepository(em);
-//    }
 }
