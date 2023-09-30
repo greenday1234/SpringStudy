c1cf855cd14a0ee18696bf3162e6c9a81c650d50
Lee ChanHui <ing3616062@naver.com>
2023-09-30 23:05:52 +0900

    주석

--- src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -43,12 +43,6 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printHeaders(HttpServletRequest request) {
         System.out.println("--- Headers - start ---");
 
-//        Enumeration<String> headerNames = request.getHeaderNames();
-//        while(headerNames.hasMoreElements()){
-//            String headerName = headerNames.nextElement();
-//            System.out.println(headerName + ": " + request.getHeader(headerName));
-//        }
-
         request.getHeaderNames().asIterator()
                         .forEachRemaining(headerName ->
                                 System.out.println(headerName + ": " + request.getHeader(headerName)));
