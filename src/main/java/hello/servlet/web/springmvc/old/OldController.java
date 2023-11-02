bc7a687e5d5275111751ad2dd0494c763db9fcc5
Lee ChanHui <ing3616062@naver.com>
2023-11-02 17:41:27 +0900

    주석

--- src/main/java/hello/servlet/web/springmvc/old/OldController.java
+++ src/main/java/hello/servlet/web/springmvc/old/OldController.java
@@ -12,6 +12,7 @@ public class OldController implements Controller {
     @Override
     public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         System.out.println("OldController.handleRequest");
+        //ModelAndView 반환
         return new ModelAndView("new-form");
     }
 }
