d26f1bae60cab9e5e022a72006933c47cd228129
Lee ChanHui <ing3616062@naver.com>
2023-11-16 19:30:45 +0900

    Command: @RestController 주석 추가

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @Slf4j
-//@Controller
-//@ResponseBody
-@RestController
+@RestController // <- @Controller + @ResponseBody
 public class ResponseBodyController {
 
     @GetMapping("/response-body-string-v1")
