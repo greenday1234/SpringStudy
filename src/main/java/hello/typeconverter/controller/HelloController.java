3c5a896cb49d400839ee7ffc5ac830f19d9f2586
Lee ChanHui <ing3616062@naver.com>
2024-01-10 21:38:53 +0900

    Feat: 웹 어플리케이션에 컨버터 적용

--- src/main/java/hello/typeconverter/controller/HelloController.java
+++ src/main/java/hello/typeconverter/controller/HelloController.java
@@ -1,5 +1,6 @@
 package hello.typeconverter.controller;
 
+import hello.typeconverter.type.IpPort;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -18,8 +19,15 @@ public class HelloController {
     }
 
     @GetMapping("/hello-v2")
-    public String helloV2(@RequestParam Integer data){
+    public String helloV2(@RequestParam("data") Integer data){
         System.out.println("data = " + data);
+        return "ok";
+    }
+
+    @GetMapping("/ip-port")
+    public String ipPort(@RequestParam("ipPort") IpPort ipPort){
+        System.out.println("ipPort IP = " + ipPort.getIp());
+        System.out.println("ipPort PORT = " + ipPort.getPort());
         return "ok";
     }
 }
