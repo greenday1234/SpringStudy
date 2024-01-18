7e1b3d001736adf296feac558892d27daa40ef36
Lee ChanHui <ing3616062@naver.com>
2024-01-18 22:48:14 +0900

    Feat: 체크 예외 문제점 예제

---
+++ src/test/java/hello/jdbc/exception/basic/CheckedAppTest.java
@@ -0,0 +1,49 @@
+package hello.jdbc.exception.basic;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.net.ConnectException;
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CheckedAppTest {
+
+    @Test
+    void checked(){
+        Controller controller = new Controller();
+        assertThatThrownBy(() -> controller.request())
+                .isInstanceOf(Exception.class);
+    }
+
+    static class Controller{
+        Service service = new Service();
+
+        public void request() throws SQLException, ConnectException {
+            service.logic();
+        }
+    }
+
+    static class Service{
+        Repository repository = new Repository();
+        NetWorkClient netWorkClient = new NetWorkClient();
+
+        public void logic() throws SQLException, ConnectException {
+            repository.call();
+            netWorkClient.call();
+        }
+    }
+
+    static class NetWorkClient{
+        public void call() throws ConnectException {
+            throw new ConnectException("연결 실패");
+        }
+    }
+
+    static class Repository{
+        public void call() throws SQLException {
+            throw new SQLException("ex");
+        }
+    }
+}
