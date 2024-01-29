97a216f2d5f30cc4b0ecdaca93d5cd84dd8b4bd3
Lee ChanHui <ing3616062@naver.com>
2024-01-29 22:35:37 +0900

    Feat: 스프링 데이터 JPA 적용 및 하이버네이트 버전 낮춤

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-@Import(JpaConfig.class)
+@Import(SpringDataJpaConfig.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
