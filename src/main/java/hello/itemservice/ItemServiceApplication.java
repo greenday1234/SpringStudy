b01a1e023b131119e259d95521a1fe2f901ece8a
Lee ChanHui <ing3616062@naver.com>
2024-01-31 21:53:53 +0900

    Feat: 실용적인 구조(SpringDataJPA 와 Querydsl 사용)

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
 
 @Slf4j
-@Import(QuerydslConfig.class)
+@Import(V2Config.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
 
