1f7669542f44e2762641f77ddb0632c712c00ddf
Lee ChanHui <ing3616062@naver.com>
2024-02-07 21:30:46 +0900

    Feat: 트랜잭션 예제 - 전파 커밋

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -1,7 +1,6 @@
 package hello.springtx.propagation;
 
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -63,6 +62,24 @@ class MemberServiceTest {
     void singleTx() {
         //given
         String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //then  :모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: ON
+     * logRepository    @Transactional: ON
+     */
+    @Test
+    void outerTxOn_success() {
+        //given
+        String username = "outerTxOn_success";
 
         //when
         memberService.joinV1(username);
