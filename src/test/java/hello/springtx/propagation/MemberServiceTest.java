2d83da8d9a9070fbe97c0022a7e8d647b7d9f64d
Lee ChanHui <ing3616062@naver.com>
2024-02-07 21:21:18 +0900

    Feat: 트랜잭션 예제 - 단일 트랜잭션

--- src/test/java/hello/springtx/propagation/MemberServiceTest.java
+++ src/test/java/hello/springtx/propagation/MemberServiceTest.java
@@ -53,4 +53,22 @@ class MemberServiceTest {
         assertTrue(memberRepository.find(username).isPresent());
         assertTrue(logRepository.find(username).isEmpty());
     }
+
+    /**
+     * memberService    @Transactional: ON
+     * memberRepository @Transactional: OFF
+     * logRepository    @Transactional: OFF
+     */
+    @Test
+    void singleTx() {
+        //given
+        String username = "singleTx";
+
+        //when
+        memberService.joinV1(username);
+
+        //then  :모든 데이터가 정상 저장된다.
+        assertTrue(memberRepository.find(username).isPresent());
+        assertTrue(logRepository.find(username).isPresent());
+    }
 }
