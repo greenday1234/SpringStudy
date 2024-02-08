0d30680654d9a0cb9d955456d45f2549d586de1a
Lee ChanHui <ing3616062@naver.com>
2024-02-08 21:55:08 +0900

    Command: 주석

--- src/test/java/hello/springtx/exception/RollbackTest.java
+++ src/test/java/hello/springtx/exception/RollbackTest.java
@@ -1,7 +1,7 @@
 package hello.springtx.exception;
 
 import lombok.extern.slf4j.Slf4j;
- import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.TestConfiguration;
--- src/test/java/hello/springtx/order/OrderServiceTest.java
+++ src/test/java/hello/springtx/order/OrderServiceTest.java
@@ -1,17 +1,13 @@
 package hello.springtx.order;
 
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.weaver.ast.Or;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
